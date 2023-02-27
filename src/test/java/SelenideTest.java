5276498d35fc59ce453984077ea5b29c7db3164e
Alex Shavlyuk <1821413@gmail.com>
2023-02-27 21:27:43 +0300

    good

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -12,13 +12,22 @@ public class SelenideTest {
     @BeforeAll
     static  void beforeAll() {
         Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;}
+        }
     @Test
     void SelenideTest(){
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
         $(".Box-row.wiki-more-pages-link").$("button").click();
         $(byText("SoftAssertions")).click();
-        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:\n"+
+                "@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();"));
     }
 }
